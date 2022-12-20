0b04400c99687e701ee2966962be84e87331bca4
AhmedYasser155 <75222161+AhmedYasser155@users.noreply.github.com>
2022-12-20 23:32:20 +0200

    routing added and intial pages created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,23 @@
-import './App.css';
-import Login from './login/login.js';
+import "./App.css";
+import Login from "./login/login.js";
+import Matches from "./matches/matches";
+import Home from "./home/home";
+import Tickets from "./tickets/tickets";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
+  //routes are defined here
   return (
     <div className="App">
-      <Login />
-     
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/matches" element={<Matches />} />
+          <Route path="/login" element={<Login />} />
+          //TODO: change this to include the match id
+          <Route path="/tickets" element={<Tickets />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
