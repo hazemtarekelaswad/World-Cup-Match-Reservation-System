025b5ebc908a2d003b7f3d006fa9c7822ff4f9e3
Saif El-Eslam <saif82820@gmail.com>
2022-12-27 20:37:11 +0200

    token saved after login and sign up request sent successfully

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,25 +8,34 @@ import Teams from "./teams/teams";
 import Stadiums from "./stadiums/stadiums";
 import Reservations from "./reservations/reservations";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { UserContext } from "./context";
+import { useContext, useState } from 'react';
 
 function App() {
+  const [token, setToken] = useState("");
+
+
   //routes are defined here
   return (
     <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/matches" element={<Matches />} />
-          <Route path="/login" element={<Login signupParam={false} />} />
-          <Route path="/signup" element={<Login signupParam={true} />} />
-          <Route path="/profile" element={<Profile />} />
-          {/* TODO: change this to include the match id */}
-          <Route path="/tickets/:id" element={<Tickets />} />
-          <Route path="/teams" element={<Teams />} />
-          <Route path="/stadiums" element={<Stadiums />} />
-          <Route path="/reservations" element={<Reservations />} />
-        </Routes>
-      </BrowserRouter>
+      <UserContext.Provider value={{token, setToken}}>
+        <BrowserRouter>
+          <Routes>
+
+            <Route path="/" element={<Home />} />  
+            <Route path="/matches" element={<Matches />} />
+            <Route path="/login" element={<Login signupParam={false} />} />
+            <Route path="/signup" element={<Login signupParam={true} />} />
+            <Route path="/profile" element={<Profile />} />
+            {/* TODO: change this to include the match id */}
+            <Route path="/tickets/:id" element={<Tickets />} />
+            <Route path="/teams" element={<Teams />} />
+            <Route path="/stadiums" element={<Stadiums />} />
+            <Route path="/reservations" element={<Reservations />} />
+
+          </Routes>
+        </BrowserRouter>
+      </UserContext.Provider>
     </div>
   );
 }
--- frontend/src/login/login.js
+++ frontend/src/login/login.js
@@ -2,20 +2,84 @@ import './login.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faUnlockKeyhole, faUser, faEnvelope, faEarthAfrica, faVenusMars } from '@fortawesome/free-solid-svg-icons'
 // import logo from '../imges/login.png';
-import { useState } from 'react';
+import { useContext, useState, useEffect } from 'react';
 import DatePicker from 'react-date-picker';
 import Dropdown from 'react-dropdown';
 import 'react-dropdown/style.css';
 import tamema from '../imges/toy.png';
 import Header from "../components/header/header";
+import { UserContext } from "../context";
+import moment from 'moment';
+import axios from "axios";
 
 
 
 function Login({signupParam}) {
+    const {token, setToken} = useContext(UserContext);
 
     const [signup, setSignup] = useState(signupParam);
     const [dateValue, onDateChange] = useState(new Date());
     // const options = [ 'fan', 'manager', 'admin' ];
+
+    const [user, setUser] = useState({
+        username: '',
+        password: '',
+        firstname: '',
+        lastname: '',
+        birthDate: '',
+        gender: '',
+        nationality: '',
+        email: '',
+        role: 'fan',
+    });
+
+    function setUserOnChange( event ) {
+        setUser({ ...user, [event.target.name]: event.target.value });
+    }
+
+    function setGenderOnChange( event ) {
+        setUser({ ...user, ['gender']: event.value == "Male" ? "M" : "F" });
+    }
+
+    function signupOnClick() {
+        setUser({ ...user, ['birthDate']: moment(dateValue).format('YYYY-MM-DD') });
+        console.log(user);
+        axios
+        .post("https://qatar2022worldcupreservationsystem.onrender.com/users/signup", {
+            "username": user.username,
+            "password": user.password,
+            "firstName": user.firstname,
+            "lastName": user.lastname,
+            "birthDate": user.birthDate,
+            "gender": user.gender,
+            "nationality": user.nationality,
+            "email": user.email,
+            "role": "fan"
+        })
+        .then((res) => {
+            console.log(res);
+            setSignup(false);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+    }
+
+    function loginOnClick() {
+        axios
+        .post("https://qatar2022worldcupreservationsystem.onrender.com/users/signin", {
+            "username": user.username,
+            "password": user.password,
+        })
+        .then((res) => {
+            setToken(res.data.token);
+            console.log(res.data.token);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+    }
+
 
     return (
     <div className="login">
@@ -33,10 +97,10 @@ function Login({signupParam}) {
                         <div className='form-wrapper'>
                             {signup && <div className='Name-container'>
                                 <div className='first-name'>
-                                    <input type='text' name='firstname' id='firstname' placeholder='First name' />
+                                    <input type='text' name='firstname' id='firstname' placeholder='First name' onChange={(e) => setUserOnChange(e)} />
                                 </div>
                                 <div className='last-name'>
-                                    <input type='text' name='lastname' id='lastname' placeholder='Last name' />
+                                    <input type='text' name='lastname' id='lastname' placeholder='Last name' onChange={(e) => setUserOnChange(e)} />
                                 </div>
                             </div>}
 
@@ -44,7 +108,7 @@ function Login({signupParam}) {
 
                             {signup && <div className='birth-date-container'>
                                 <div className='label'>Birth date: </div>
-                                <DatePicker className="DatePicker" onChange={onDateChange} value={dateValue} />
+                                <DatePicker className="DatePicker" name='birthDate' format='dd/MM/yyyy' onChange={onDateChange} value={dateValue} />
                             </div>}
 
                             
@@ -57,7 +121,7 @@ function Login({signupParam}) {
                                     <FontAwesomeIcon className='icon-item' icon={faEarthAfrica} />
                                 </div>
                                 <div className='input'>
-                                    <input type='text' name='nationality' id='nationality' placeholder='nationality' />
+                                    <input type='text' name='nationality' id='nationality' placeholder='nationality' onChange={(e) => setUserOnChange(e)}/>
                                 </div>
                             </div>}
 
@@ -66,7 +130,7 @@ function Login({signupParam}) {
                                     <FontAwesomeIcon className='icon-item' icon={faEnvelope} />
                                 </div>
                                 <div className='input'>
-                                    <input type='email' name='email' id='email' placeholder='email' />
+                                    <input type='email' name='email' id='email' placeholder='email' onChange={(e) => setUserOnChange(e)}/>
                                 </div>
                             </div>}
 
@@ -75,7 +139,8 @@ function Login({signupParam}) {
                                     <FontAwesomeIcon className='icon-item' icon={faVenusMars} />
                                 </div>
                                 <div className='gender'>
-                                    <Dropdown className="Dropdown" options={['Male', 'Female']} value={''} placeholder="Select your gender" />
+                                    <Dropdown className="Dropdown" name='gender' options={['Male', 'Female']} value={''} 
+                                    placeholder="Select your gender" onChange={(e) => setGenderOnChange(e)}/>
                                 </div>
                             </div>}
 
@@ -84,7 +149,7 @@ function Login({signupParam}) {
                                     <FontAwesomeIcon className='icon-item' icon={faUser} />
                                 </div>
                                 <div className='input'>
-                                    <input type='text' name='username' id='username' placeholder='username' />
+                                    <input type='text' name='username' id='username' placeholder='username' onChange={(e) => setUserOnChange(e)}/>
                                 </div>
                             </div>
                             <div className='password-container'>
@@ -92,18 +157,23 @@ function Login({signupParam}) {
                                     <FontAwesomeIcon className='icon-item' icon={faUnlockKeyhole} />
                                 </div>
                                 <div className='input'>
-                                    <input type='password' name='password' id='password' placeholder='password' />
+                                    <input type='password' name='password' id='password' placeholder='password' onChange={(e) => setUserOnChange(e)}/>
                                 </div>
                             </div>
+                            {signup && <div className='choose-role'>
+                                <div className='choose-role-container'>
+                                    <div className='label'>Sign Up as admin </div>
+                                    <input type='checkbox' name='role' id='role' onChange={(e) => setUserOnChange(e.target.value ? "manager" : "fan")}/>
+                                </div>
+                            </div>}
                         </div>
                     </div>
                     <div className='footer'>
+                        <div className='button-container'>
+                            <button onClick={()=>{{!signup && loginOnClick();} {signup && signupOnClick();} }}>{signup ? 'SignUp' : 'Login'}</button>
+                        </div>
                         <div className='change-mode' onClick={() => setSignup(!signup)}>
                             {signup ? 'Login' : 'SignUp'}
-                        </div>
-
-                        <div className='button-container'>
-                            <button>{signup ? 'SignUp' : 'Login'}</button>
                         </div>
                     </div>
                 </div>
