71d302a6daf09f3aa46b6d762f80490eee9499bc
Hazem Tarek Elaswad <hazem.elaswad00@eng-st.cu.edu.eg>
2022-12-29 00:59:56 +0200

    fix: change endpoint name

--- backend/src/routes/match-route.js
+++ backend/src/routes/match-route.js
@@ -8,7 +8,7 @@ const mangercontroller = require('../controllers/match-controller')
 router.get('/matches', mangercontroller.getAllMatches)
 router.get('/matches/:id', mangercontroller.getMatch)
 
-router.patch('/manager/match/:id', auth.verifyToken, mangercontroller.updateMatch)
+router.put('/matches/:id', auth.verifyToken, mangercontroller.updateMatch)
 router.post('/manager/match', auth.verifyToken, mangercontroller.createMatch)
 router.delete('/manager/match/:id', auth.verifyToken, mangercontroller.deleteMatch)
 
